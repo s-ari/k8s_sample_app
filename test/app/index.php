7f978237e40de9bc890cb47d112743ead268fccc
s-ari <sgt.arima@gmail.com>
2020-09-03 16:37:03 +0900

    move docker files

---
+++ test/app/index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<meta charset=UFT-8>
+<title>Blue Green App</title>
+<style>
+    body {
+        background-image: linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%);
+    }
+      
+    .word {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    
+        padding: 15px 30px;
+        text-align: center;
+        width: 90%;
+    }
+</style>
+</head>
+<body>
+    <?php
+        $color = "blue";
+
+        $word = getenv('OUTPUT_WORD');
+        echo '<div class=word>';
+        echo '<font size="7" style="color:'. $color . '"><b>' . $word . '</b>';
+        echo '</div>';
+    ?>
+</body>
+</html>
+	
